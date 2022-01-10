c792b6889c7927f280b3b1a83116a4cde5b8dde7
kwaneung <whatsup95@naver.com>
2022-01-10 22:04:15 +0900

    init
    프로젝트 생성

---
+++ kwaneung/src/main/java/com/test/kwaneung/KwaneungApplication.java
@@ -0,0 +1,13 @@
+package com.test.kwaneung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwaneungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwaneungApplication.class, args);
+	}
+
+}
---
+++ kwaneung/src/test/java/com/test/kwaneung/KwaneungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kwaneung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwaneungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
